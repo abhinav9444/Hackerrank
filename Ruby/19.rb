8c95ccba4abf3d37700482208d96d483f4b90ef4
abhinav9444 <abhinavkrsingh9444@gmail.com>
2024-11-06 11:29:03 +0530

    new commit

---
+++ Ruby/19.rb
@@ -0,0 +1,10 @@
+# Problem Link  : https://www.hackerrank.com/challenges/ruby-enumerable-introduction/problem?isFullScreen=true
+
+def iterate_colors(colors)
+    # Your code here
+      arr=[]
+      colors.each do |items|
+          arr.push(items)
+      end
+      return arr
+  end
